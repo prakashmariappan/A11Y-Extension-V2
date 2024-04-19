fb174f4f6b00fc6e5f6c79deb4e19628f27aa667
prakash <prakashmariappan09@gmail.com>
2024-04-18 22:47:15 -0500

    Codecopy

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ const MainHTML = `
 <body>
 <div id="common">
 <div id="toggleButton">
-<img class="icon-btn" alt="Logo"  src="${chrome.runtime.getURL("defaultLogo.png")}">
+<img class="icon-btn" alt="Logo"  src="${chrome.runtime.getURL(
+  "defaultLogo.png"
+)}">
 </div>
 <div class="container" id="a11y-extension" style="display: none;">
     <div class="welcome-box">
-        <img class="logo" alt="Logo" id="reload" src="${chrome.runtime.getURL("logo.png")}">
+        <img class="logo" alt="Logo" id="reload" src="${chrome.runtime.getURL(
+          "logo.png"
+        )}">
     </div>
     <div class="desc">Our chatbot teaches you how to build Websites and Apps accessible to all. Let's make the digital world welcoming for all. Start your easy-to-follow journey with us today!</div>
     <div class="media-list"></div>
@@ -29,75 +33,74 @@ const MainHTML = `
 </div>
 </body>
 `;
-document.body.insertAdjacentHTML('afterbegin', MainHTML);
-    
-     // Make the container draggable
-     const container = document.getElementById('common');
-   
-     let containerDragging = false;
-     let containerOffsetX, containerOffsetY;
- 
-     container.addEventListener('mousedown', startContainerDrag);
-     document.addEventListener('mouseup', endDrag);
- 
-     function startContainerDrag(e) {
-         containerDragging = true;
-         containerOffsetX = e.clientX - container.getBoundingClientRect().left;
-         containerOffsetY = e.clientY - container.getBoundingClientRect().top;
-     }
+document.body.insertAdjacentHTML("afterbegin", MainHTML);
 
-     function adjustContainerPosition() {
-        const windowWidth = window.innerWidth;
-        const windowHeight = window.innerHeight;
-        const containerRect = container.getBoundingClientRect();
+// Make the container draggable
+const container = document.getElementById("common");
 
-        if (containerRect.left < 0) {
-            container.style.left = '0';
-        } else if (containerRect.right > windowWidth) {
-            container.style.left = `${windowWidth - containerRect.width}px`;
-        }
+let containerDragging = false;
+let containerOffsetX, containerOffsetY;
 
-        if (containerRect.top < 0) {
-            container.style.top = '0';
-        } else if (containerRect.bottom > windowHeight) {
-            container.style.top = `${windowHeight - containerRect.height}px`;
-        }
-    }
+container.addEventListener("mousedown", startContainerDrag);
+document.addEventListener("mouseup", endDrag);
 
-     document.addEventListener('mousemove', e => {
-         if (containerDragging) {
-             container.style.left = `${e.clientX - containerOffsetX}px`;
-             container.style.top = `${e.clientY - containerOffsetY}px`;   
-             container.style.userSelect = 'none'; // Prevent text selection during dragging
-         }
-     });
+function startContainerDrag(e) {
+  containerDragging = true;
+  containerOffsetX = e.clientX - container.getBoundingClientRect().left;
+  containerOffsetY = e.clientY - container.getBoundingClientRect().top;
+}
 
-     function endDrag() {
-        containerDragging = false;
-        adjustContainerPosition(); // Call adjustContainerPosition() while dragging
-        container.style.userSelect = ''; // Allow text selection after dragging
-    }
+function adjustContainerPosition() {
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const containerRect = container.getBoundingClientRect();
 
+  if (containerRect.left < 0) {
+    container.style.left = "0";
+  } else if (containerRect.right > windowWidth) {
+    container.style.left = `${windowWidth - containerRect.width}px`;
+  }
 
-     let message = "";
+  if (containerRect.top < 0) {
+    container.style.top = "0";
+  } else if (containerRect.bottom > windowHeight) {
+    container.style.top = `${windowHeight - containerRect.height}px`;
+  }
+}
 
-     const setMessage = (value) => message = value;
+document.addEventListener("mousemove", (e) => {
+  if (containerDragging) {
+    container.style.left = `${e.clientX - containerOffsetX}px`;
+    container.style.top = `${e.clientY - containerOffsetY}px`;
+    container.style.userSelect = "none"; // Prevent text selection during dragging
+  }
+});
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        const message = document.getElementById('messageText').value.trim();
-        if (message !== "") {
-            try {
-                const descElement = document.querySelector('.desc');
-                if (descElement) {
-                    descElement.remove();
-                }
+function endDrag() {
+  containerDragging = false;
+  adjustContainerPosition(); // Call adjustContainerPosition() while dragging
+  container.style.userSelect = ""; // Allow text selection after dragging
+}
 
-                const userMessage = document.getElementById('messageText').value;
-                document.getElementById('messageText').value = '';
+let message = "";
 
-                const mediaList = document.querySelector('.media-list');
-                mediaList.innerHTML += `
+const setMessage = (value) => (message = value);
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const message = document.getElementById("messageText").value.trim();
+  if (message !== "") {
+    try {
+      const descElement = document.querySelector(".desc");
+      if (descElement) {
+        descElement.remove();
+      }
+
+      const userMessage = document.getElementById("messageText").value;
+      document.getElementById("messageText").value = "";
+
+      const mediaList = document.querySelector(".media-list");
+      mediaList.innerHTML += `
                     <li class="media">
                         <div class="usermessage">
                             <div class="author"><b> You </b><div class='userinput_box'>${userMessage}</div></div>
@@ -110,57 +113,97 @@ document.body.insertAdjacentHTML('afterbegin', MainHTML);
                     </li>
                 `;
 
-                mediaList.scrollTop = mediaList.scrollHeight;
+      mediaList.scrollTop = mediaList.scrollHeight;
 
-                const data = {
-                    input_text: userMessage,
-                    chat_id: "de305d54-75b4-431b-adb2-eb6b9e546014"
-                };
+      const data = {
+        input_text: userMessage,
+        chat_id: "de305d54-75b4-431b-adb2-eb6b9e546014",
+      };
 
-                const response = await fetch("https://api.botsonic.ai/v1/botsonic/generate", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "token": "25f9a329-2f2a-43a8-a5b9-45cddb7d5589"
-                    },
-                    body: JSON.stringify(data)
-                });
+      const response = await fetch(
+        "https://api.botsonic.ai/v1/botsonic/generate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            token: "25f9a329-2f2a-43a8-a5b9-45cddb7d5589",
+          },
+          body: JSON.stringify(data),
+        }
+      );
 
-                const responseData = await response.json();
-                const answer = responseData.answer;
-                let contentAnswer;
-                if (answer.includes('**Answer:**')) {
-                    contentAnswer = answer.split('**Answer:**')[1].trim();
-                } else {
-                    contentAnswer = answer;
-                }
+      const responseData = await response.json();
+      const answer = responseData.answer;
+      let highlightedAnswer;
+      if (answer) {
+        highlightedAnswer = answer.replace(
+          /```(.*?)```/g,
+          '<div class="code-con"><div class="code-head"><span class="code-text">Copy code</span></div><div class="code-body">$1</div></div>'
+        );
+      } else {
+        highlightedAnswer = answer;
+      }
 
-                document.querySelectorAll('.type').forEach(elem => elem.remove());
+      document.querySelectorAll(".type").forEach((elem) => elem.remove());
 
-                mediaList.innerHTML += `
+      mediaList.innerHTML += `
                     <li class="media">
                         <div class="answer">
-                            <div class="bot"><b> A11Y Chatbot </b><div class='botoutput_box'>${contentAnswer}</div></div>
+                            <div class="bot"><b> A11Y Chatbot </b><div class='botoutput_box'>${highlightedAnswer}</div></div>
                         </div>
                     </li>
                 `;
+      document.addEventListener("click", function (event) {
+        const clickedElement = event.target;
 
-                mediaList.scrollTop = mediaList.scrollHeight;
-            } catch (error) {
-                console.error(error);
+        // Check if the clicked element is a .code-text element
+        if (clickedElement.classList.contains("code-text")) {
+          // Find the closest parent element with class .code-con
+          const codeContainer = clickedElement.closest(".code-con");
+
+          // If a valid code container is found
+          if (codeContainer) {
+            // Find the code body within the code container
+            const codeBody = codeContainer.querySelector(".code-body");
+
+            // If a valid code body is found
+            if (codeBody) {
+              // Copy the text inside code body to clipboard
+              navigator.clipboard
+                .writeText(codeBody.textContent)
+                .then(function () {
+                  // Once text is copied, update the code-text to "Copied!"
+                  clickedElement.textContent = "Copied!";
+                  // Reset text to "Copy code" after 2 seconds
+                  setTimeout(function () {
+                    clickedElement.textContent = "Copy code";
+                  }, 2000);
+                })
+                .catch(function (err) {
+                  console.error("Failed to copy text: ", err);
+                });
             }
+          }
         }
+      });
+      mediaList.scrollTop = mediaList.scrollHeight;
+    } catch (error) {
+      console.error(error);
     }
+  }
+}
 
-    document.getElementById('chatbot-form').addEventListener('submit', handleSubmit);
+document
+  .getElementById("chatbot-form")
+  .addEventListener("submit", handleSubmit);
 
-    // Toggle visibility of container on click
-    const logo = document.getElementById('a11y-extension');
-    const toggleButton = document.getElementById('toggleButton');
-    toggleButton.addEventListener('click', function() {
-        if (logo.style.display === 'none') {
-            logo.style.display = 'flex';
-        } else {
-            logo.style.display = 'none';
-        }
-    });
+// Toggle visibility of container on click
+const logo = document.getElementById("a11y-extension");
+const toggleButton = document.getElementById("toggleButton");
+toggleButton.addEventListener("click", function () {
+  if (logo.style.display === "none") {
+    logo.style.display = "flex";
+  } else {
+    logo.style.display = "none";
+  }
+});
